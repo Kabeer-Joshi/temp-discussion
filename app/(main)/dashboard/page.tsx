6183c8d93ec9013e42d9d5e061ef83b0a7f759a6
Kabeer-Joshi <kabeer@goreeva.com>
2024-11-04 15:01:31 +0530

    First commit

---
+++ app/(main)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Page() {
+  return (
+    <div className="font-semibold text-2xl bg-gray-200 ">
+      This is a protected route 🔒
+    </div>
+  )
+}
---
+++ app/(main)/temp/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="font-semibold text-2xl bg-gray-200 ">
+      Temporary page  🔒
+    </div>
+  )
+}
